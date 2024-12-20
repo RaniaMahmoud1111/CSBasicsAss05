358a4d85dc57dc24869bb4b3460ce5447c0ff64a
Rania Mahmoud <Rania.Mahm555@gmail.com>
2024-12-21 01:04:31 +0200

    add question 19

--- CSBasicsAss05/Program.cs
+++ CSBasicsAss05/Program.cs
@@ -4,31 +4,32 @@
     {
         static void Main(string[] args)
         {
-
             #region 19. Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
-            int n;
-            Console.Write("enter number represent size of identity Matrix: ");
-            n=int.TryParse(Console.ReadLine(), out n)? n:0;
+            //int n;
+            //Console.Write("enter number represent size of identity Matrix: ");
+            //n = int.TryParse(Console.ReadLine(), out n) ? n : 0;
 
-            int [,] matrix=new int [n,n];
-            if (n > 0)
-            {
-                Console.Write("[");
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < n; j++)
-                    {
-                        if (i == j)
-                            matrix[i, j] = 1;
+            //int[,] matrix = new int[n, n];
+            //if (n > 0)
+            //{
+            //    Console.Write("[");
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        for (int j = 0; j < n; j++)
+            //        {
+            //            if (i == j)
+            //                matrix[i, j] = 1;
 
-                        Console.Write($"{matrix[i, j]} ");
-                    }
-                    Console.WriteLine("\n");
-                }
-                Console.Write("]");
-            }
+            //            Console.Write($"{matrix[i, j]} ");
+            //        }
+            //        Console.WriteLine("\n");
+            //    }
+            //    Console.Write("]");
+            //}
 
             #endregion
+
+
         }
     }
 }
